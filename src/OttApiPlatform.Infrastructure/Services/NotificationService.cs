a180ec366d0274421905ad5afd03df3834df7e5e
Dave Evans <60859872+davewevans@users.noreply.github.com>
2024-08-25 14:32:10 -0400

    Add Azure email communication service for email confirmation

--- src/OttApiPlatform.Infrastructure/Services/NotificationService.cs
+++ src/OttApiPlatform.Infrastructure/Services/NotificationService.cs
@@ -1,7 +1,23 @@
-﻿namespace OttApiPlatform.Infrastructure.Services;
+﻿using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Communication;
+using Azure.ResourceManager.Resources;
+using Azure.Communication.Email;
+
+namespace OttApiPlatform.Infrastructure.Service;
 
 public class NotificationService : INotificationService
 {
+
+    private readonly IConfigReaderService _configReaderService;
+
+    public NotificationService(IConfigReaderService configReaderService)
+    {
+        _configReaderService = configReaderService;
+    }
+
     #region Public Methods
 
     public Task SendSmsAsync(Message message)
@@ -9,27 +25,51 @@ public class NotificationService : INotificationService
         return Task.CompletedTask;
     }
 
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        //MailAddress to = new MailAddress(email);
-        //MailAddress from = new MailAddress(_configReaderService.GetSmtpOption().From);
+        // ref: https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/email/send-email?tabs=windows%2Cconnection-string%2Csend-email-and-get-status-async%2Csync-client&pivots=programming-language-csharp
 
-        //MailMessage message = new MailMessage(from, to);
-        //message.Subject = subject;
-        //message.Body = htmlMessage;
-        //message.IsBodyHtml = true;
-        //SmtpClient client = new SmtpClient.
-        //{
-        //    Host = _configReaderService.GetSmtpOption().SmtpServer,//"smtp.gmail.com",
-        //    Port = _configReaderService.GetSmtpOption().Port,
-        //    EnableSsl = _configReaderService.GetSmtpOption().EnableSsl.
-        //};
+        var smtpOptions = _configReaderService.GetSmtpOption();
+        string connectionString = smtpOptions.AzureCommunicationServicesConnectionString;
+        EmailClient emailClient = new EmailClient(connectionString);
+        var sender = smtpOptions.Sender;
 
-        //client.UseDefaultCredentials = _configReaderService.GetSmtpOption().UseDefaultCredentials;
-        //client.Credentials = new NetworkCredential(_configReaderService.GetSmtpOption().UserName, _configReaderService.GetSmtpOption().Password);
-        //client.Send(message);
+        // Send the email message with WaitUntil.Started
+        EmailSendOperation emailSendOperation = await emailClient.SendAsync(
+            Azure.WaitUntil.Started,
+            sender,
+            email,
+            subject,
+            htmlMessage);
 
-        return Task.CompletedTask;
+        // Call UpdateStatus on the email send operation to poll for the status manually.       
+        try
+        {
+            while (true)
+            {
+                await emailSendOperation.UpdateStatusAsync();
+                if (emailSendOperation.HasCompleted)
+                {
+                    break;
+                }
+                await Task.Delay(100);
+            }
+
+            if (emailSendOperation.HasValue)
+            {
+                // TODO log
+                Console.WriteLine($"Email queued for delivery. Status = {emailSendOperation.Value.Status}");
+            }
+        }
+        catch (RequestFailedException ex)
+        {
+            // TODO log
+            Console.WriteLine($"Email send failed with Code = {ex.ErrorCode} and Message = {ex.Message}");
+        }
+
+        /// Get the OperationId so that it can be used for tracking the message for troubleshooting
+        string operationId = emailSendOperation.Id;
+        Console.WriteLine($"Email operation id = {operationId}");
     }
 
     #endregion Public Methods
